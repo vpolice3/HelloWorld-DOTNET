9733a465248b25dac53e0db55ffc1d92acf1486f
vpolice3 <60419673+vpolice3@users.noreply.github.com>
2020-06-10 19:26:33 +0530

    again checking the unit testing for jenkins to build

--- HelloWorldTests/UnitTest1.cs
+++ HelloWorldTests/UnitTest1.cs
@@ -10,7 +10,7 @@ namespace HelloWorldTests
         [TestMethod]
         public void TestMethod1()
         {
-            Assert.AreEqual("My World", Program.CreateMessage());
+            Assert.AreEqual("Hello World", Program.CreateMessage());
         }
     }
 }
